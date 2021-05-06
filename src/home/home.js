60e081f0ae5c4833838eb49e6d07634363e0c245
Vitor Lourenco <vitor.registros@lourencos.net>
2021-05-06 19:39:57 -0300

    Comment home.js

--- src/home/home.js
+++ src/home/home.js
@@ -5,6 +5,9 @@ import React from "react";
 
 export default function Home({ orderState, setOrderState }) {
   function handleOrderStatus() {
+
+    //isReady is true if at least 1 item per category
+    //has a property amount that is larger than zero
     const isReady = orderState.reduce((bol, row) => {
       let rowFlag = false;
       row.forEach((item) => {
@@ -13,19 +16,18 @@ export default function Home({ orderState, setOrderState }) {
       return bol && rowFlag;
     }, true);
 
-    if (isReady === true) {
-      if (orderStatus === false) {
-        setOrderStatus(true);
-      }
-    } else {
-      if (orderStatus === true) {
-        setOrderStatus(false);
-      }
-    }
+    //only setOrderStatus if orderStatus and isReady arent matching
+    if (isReady !== orderStatus) setOrderStatus(isReady);
+
   }
 
+  //orderStatus only job is to know wether or not the 'Fechar pedido'
+  //button should be displayed. It has nothing to do with orderState
+  //orderSate is a state of <App /> that keeps track of the users choices
   const [orderStatus, setOrderStatus] = React.useState(false);
 
+  //Everytime orderState (state of <App />) is updated, the function Home
+  //runs again, so i'm checking if i should display 'Fechar pedido' or not
   handleOrderStatus();
 
   return (
